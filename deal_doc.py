e42c2b923fa8be51629db2af72b1b793c96c7681
florije1988 <florije1988@gmail.com>
2016-03-04 19:51:11 +0800

    格式化代码。

--- deal_doc.py
+++ deal_doc.py
@@ -3,4 +3,7 @@ __author__ = 'florije'
 from docx import Document
 
 document = Document('test.docx')
-
+author = document.core_properties.author
+print(author)
+for paragraph in document.paragraphs:
+    print(paragraph.text)
